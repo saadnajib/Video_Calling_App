6b94ac218423f00f72ae74a504ff93cd097cd3f8
saadnajib <saadnajib97@hotmail.com>
2020-12-21 03:42:35 +0500

    added pages and designed registration page

---
+++ src/pages/Events.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Events extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Events</h1>
+      </div>
+    );
+  }
+}
+
+export default Events;
---
+++ src/pages/Gallery.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Gallery extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Gallery</h1>
+      </div>
+    );
+  }
+}
+
+export default Gallery;
